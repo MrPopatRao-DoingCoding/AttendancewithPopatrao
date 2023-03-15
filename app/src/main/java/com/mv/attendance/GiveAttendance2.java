a623981d9874bd66b93d93cf63d1adc1c67b3b3f
MrPopatRao-DoingCoding <codingwithpopatrao@gmail.com>
2023-03-15 22:43:37 +0530

    first commit

---
+++ app/src/main/java/com/mv/attendance/GiveAttendance2.java
@@ -0,0 +1,30 @@
+package com.mv.attendance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GiveAttendance2 extends AppCompatActivity {
+    TextView idTVScannedData2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_give_attendance2);
+    }
+
+    //After scanning successfully ://
+
+
+
+
+
+
+
+
+
+
+
+}
