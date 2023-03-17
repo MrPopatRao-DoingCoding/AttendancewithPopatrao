0bf24484a649075d6a72eb1edd8a4ac8335b2a9a
MrPopatRao-DoingCoding <codingwithpopatrao@gmail.com>
2023-03-17 14:17:29 +0530

    First Commit 8 - MV

--- app/src/main/java/com/mv/attendance/HomeActivity.java
+++ app/src/main/java/com/mv/attendance/HomeActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Button giveAttendance, takeAttendance, settings;
+    ImageView setting_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class HomeActivity extends AppCompatActivity {
         giveAttendance = findViewById(R.id.idBtoGiveAttendanceActivityHome);
         takeAttendance = findViewById(R.id.idBtoTakeAttendance);
         //settings = findViewById(R.id.setting_image);
+        setting_image = findViewById(R.id.setting_image);
 
         giveAttendance.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +34,14 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomeActivity.this, TakeAttendanceList.class);
+                startActivity(intent);
+            }
+        });
+
+        setting_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
                 startActivity(intent);
             }
         });
