6f007c40df52351d51335114feed4a709d4d68a1
MrPopatRao-DoingCoding <codingwithpopatrao@gmail.com>
2023-03-18 09:09:41 +0530

    First Commit 11 - MV

--- app/src/main/java/com/mv/attendance/AttendanceListViewMode2.java
+++ app/src/main/java/com/mv/attendance/AttendanceListViewMode2.java
@@ -1,13 +1,24 @@
 package com.mv.attendance;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +34,7 @@ public class AttendanceListViewMode2 extends AppCompatActivity {
         setContentView(R.layout.activity_attendance_list_view_mode2);
 
         listViewPast=findViewById(R.id.listViewPast);
-        //new_attendance_session_button=findViewById(R.id.buttonNewAttendance);
+        new_attendance_session_button=findViewById(R.id.buttonNewAttendanceMode2ThroughQRCode);
 
         List<String> ListElementsArrayList = new ArrayList<>();
         //ListElementsArrayList.add("Hello");
@@ -56,6 +67,66 @@ public class AttendanceListViewMode2 extends AppCompatActivity {
                 }
             }
         );
+
+        new_attendance_session_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(AttendanceListViewMode2.this);
+                LinearLayout layout = new LinearLayout(AttendanceListViewMode2.this);
+                layout.setBackgroundColor(Color.parseColor("#EEEEEE"));
+                Drawable drawable = ContextCompat.getDrawable(AttendanceListViewMode2.this,  R.drawable.white_alert_dialogue_background);
+                layout.setBackground(drawable);
+                layout.setPadding(20,20,20,0);
+                layout.setOrientation(LinearLayout.VERTICAL);
+                layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
+
+                final EditText inputEditTextTitle = new EditText(getApplicationContext());
+                final EditText inputEditTextNameOfTeacher = new EditText(getApplicationContext());
+                //final EditText inputEditTextRollNoOfStudent = new EditText(getApplicationContext());
+                inputEditTextTitle.setHint("Title");
+                inputEditTextNameOfTeacher.setHint("Teacher Name");
+                //inputEditTextRollNoOfStudent.setHint("Roll No.");
+                //inputEditTextRollNoOfStudent.setInputType(InputType.TYPE_CLASS_NUMBER);
+                layout.addView(inputEditTextTitle);
+                layout.addView(inputEditTextNameOfTeacher);
+                //layout.addView(inputEditTextRollNoOfStudent);
+                builder.setMessage("Add a new lecture")
+                        .setCancelable(false)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                /*Student student = new Student(Integer.parseInt(inputEditTextRollNoOfStudent.getText().toString()), inputEditTextDivOfStudent.getText().toString(), inputEditTextNameOfStudent.getText().toString());
+                                finalListAttendanceSession[0].get(countInListAttendance).addStudent(student);
+                                Gson gson = new Gson();
+                                String jsonToShareAttendanceSession = gson.toJson(attendanceSession);
+
+
+                                String jsonToListElementsAdapterList= gson.toJson(finalListAttendanceSession[0]);
+                                myEdit.putString("ListAttendanceSession", jsonToListElementsAdapterList);
+                                myEdit.apply();
+                                mainDisplayAttendanceTextView.setText(finalListAttendanceSession[0].get(countInListAttendance).generateStringNonRepeatative());*/
+                                dialog.dismiss();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //  Action for 'NO' Button
+                                dialog.cancel();
+                            }
+                        });
+                //Creating dialog box
+                AlertDialog alert = builder.create();
+                //Setting the title manually
+                alert.setTitle("New Attendance Session");
+                alert.setView(layout);
+                drawable = ContextCompat.getDrawable(AttendanceListViewMode2.this,  R.drawable.grey_alert_dialogue_background);
+                alert.getWindow().setBackgroundDrawable(drawable);
+                alert.show();
+                WindowManager.LayoutParams lp = alert.getWindow().getAttributes();
+                lp.dimAmount = 0.75f;
+                alert.getWindow().setAttributes(lp);
+            }
+        });
 
     }
 }
