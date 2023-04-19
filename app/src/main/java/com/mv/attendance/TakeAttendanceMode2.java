f0c7ff76a9b3f67faf2fe86988a318ac4ad4f6ae
MrPopatRao-DoingCoding <codingwithpopatrao@gmail.com>
2023-04-19 12:43:38 +0530

    First Commit 52 - MV       Beautification - TakeAttendanceMode2.java

--- app/src/main/java/com/mv/attendance/TakeAttendanceMode2.java
+++ app/src/main/java/com/mv/attendance/TakeAttendanceMode2.java
@@ -2,7 +2,6 @@ package com.mv.attendance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -15,21 +14,23 @@ public class TakeAttendanceMode2 extends AppCompatActivity {
 
 
     ImageView setting_image;
-    Button idBtoGiveAttendance2;
-    Button idBtoTakeAttendance2;
-    @SuppressLint("MissingInflatedId")
+    Button buttonToGiveAttendance2;
+    Button buttonToTakeAttendance2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_attendance_mode2);
 
+        // Link variables with XML Views
         setting_image = findViewById(R.id.setting_image);
-        idBtoGiveAttendance2 = findViewById(R.id.idBtoGiveAttendance2);
-        idBtoTakeAttendance2 = findViewById(R.id.idBtoTakeAttendance2);
+        buttonToGiveAttendance2 = findViewById(R.id.idBtoGiveAttendance2);
+        buttonToTakeAttendance2 = findViewById(R.id.idBtoTakeAttendance2);
 
+        // Shared Preferences
         SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
 
-        setting_image.setOnClickListener(new View.OnClickListener() {
+        setting_image.setOnClickListener(new View.OnClickListener() {  // Settings Image clicked, open Settings activity
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(TakeAttendanceMode2.this, SettingsActivity.class);
@@ -37,14 +38,14 @@ public class TakeAttendanceMode2 extends AppCompatActivity {
             }
         });
 
-        idBtoGiveAttendance2.setOnClickListener(new View.OnClickListener() {
+        buttonToGiveAttendance2.setOnClickListener(new View.OnClickListener() {  // If settings changed before delta time, then only allow to give attendance
             @Override
             public void onClick(View v) {
-                if(Math.abs(sh.getLong("savedTime", 167666442) - getTime()) > 20) {   ////////Change time here!!
+                if(Math.abs(sh.getLong("savedTime", 167666442) - getTime()) > 20) {  // Check delta-time condition   ////////Change time here!!
                     Intent intent = new Intent(TakeAttendanceMode2.this, GiveAttendance2.class);
                     startActivity(intent);
                 }
-                else{
+                else{  // Settings changed in less than delta time, show error toast.
                     Toast.makeText(TakeAttendanceMode2.this, "Not enough time has passed since you changed the settings!", Toast.LENGTH_SHORT).show();
                 }
 
@@ -52,7 +53,7 @@ public class TakeAttendanceMode2 extends AppCompatActivity {
             }
         });
 
-        idBtoTakeAttendance2.setOnClickListener(new View.OnClickListener() {
+        buttonToTakeAttendance2.setOnClickListener(new View.OnClickListener() {  // Mode 2 Attendance List
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(TakeAttendanceMode2.this, AttendanceListViewMode2.class);
@@ -63,7 +64,7 @@ public class TakeAttendanceMode2 extends AppCompatActivity {
 
     }
 
-    private long getTime() {
+    private long getTime() {  // Return current UNIX Timestamp in seconds
         return System.currentTimeMillis() / 1000L;
     }
 }
